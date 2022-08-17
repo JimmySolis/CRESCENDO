a9dde97bf96b5eb4d886d41be25e8b72e909be22
Baileigh <brownbaileigh1@gmail.com>
2022-08-17 18:59:24 -0400

    all new php platform

--- README.md
+++
@@ -1,8 +0,0 @@
-  * 01: CRESCENDO 
-    * [Baileigh Brown] | Slack name: [Baileigh Brown] | Github : [bai1eigh]
-    * [Olivia Guerra] | Slack name: [Olivia Guerra] | Github : [oguerra9]
-    * [Mayowa Aderibigbe] | Slack name: [Mayowa Aderibigbe] | Github : [mayowa320]
-    * [Jimmy Solis] | Slack name: [Jimmy Solis] | Github : [JimmySolis]
-    * [Emiliano A.] | Slack name: [Emiliano A.] | Github : [EA15243]
-    * Heroku Deployed Link: [HEROKU HERE]
-    * Project Github Repo: [https://github.com/JimmySolis/CRESCENDO]
---
+++ includes/connection.php
@@ -0,0 +1,21 @@
+<?php 
+//local conncections/development 
+$con = mysqli_connect("localhost", "root", "","social_crescendo");
+
+//remote connection (not on my computer)
+//mysql://dunc6wcjy59n9yyt:bwv3oqrsrehf2yax@n4m3x5ti89xl6czh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/qovu2bmfjl7e3pnd
+
+//$servername = "n4m3x5ti89xl6czh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+//$username = "n4m3x5ti89xl6czh";
+//$password = "cbetxkdyhwsb";
+//$db = "qovu2bmfjl7e3pnd";
+//// Create connection
+//$con = mysqli_connect($servername, $username, $password,$db);
+//
+//// Check connection
+//if (!$conn) {
+// die("Connection failed: " . mysqli_connect_error());
+//}
+//echo "Connected successfully";
+?>
+
